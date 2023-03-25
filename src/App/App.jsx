5aa5550113ecf4fc044eacbe8bc994cbd86d070f
Andrii Beznosov <andrey301288@gmail.com>
2023-03-25 21:05:00 +0200

    fix error eslink

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -11,14 +11,20 @@ import { React } from '../pages/React';
 import { HomeTestingHistory } from '../components/Home/HomeTestingHistory/HomeTestingHistory';
 
 const App = () => {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin] = useState(true);
   const [status, setAction] = useState(null);
   return isLogin ? (
     <div>
       <Routes>
-        <Route path="/" element={<Layout status={status} setAction={setAction} />}>
+        <Route
+          path="/"
+          element={<Layout status={status} setAction={setAction} />}
+        >
           <Route index element={<HomeTestingHistory status={status} />} />
-          <Route path="testing-history" element={<TestingHistory status={status} />} />
+          <Route
+            path="testing-history"
+            element={<TestingHistory status={status} />}
+          />
           <Route path="html-css" element={<HtmlCss status={status} />} />
           <Route path="javascript" element={<JavaScript status={status} />} />
           <Route path="react" element={<React status={status} />} />
