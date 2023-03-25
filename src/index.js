5df2ccaa152ad9604c9fe1e81fa5005ee217ba23
Andrii Beznosov <andrey301288@gmail.com>
2023-03-25 23:12:04 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { theme } from './constants';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/know_ukraine">
+  <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
